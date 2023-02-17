60e6d23792c4b71965cc73876611ba95d90e2eb2
dima-in <dima-in@mail.ru>
2023-02-17 20:19:12 +0300

    добавлен endpoint view() и HTML страница для фильтрации запросов в базу данных

--- BDalchemy.py
+++ BDalchemy.py
@@ -1,18 +1,30 @@
+import asyncio
+import aiohttp
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, Float, Date, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
+
+"""создаем базовый класс"""
 Base = declarative_base()
+
+"""создаем соеденение с базой данных"""
 engine = create_engine('mysql+mysqlconnector://oilsquiser:oilsquiserpasswd@localhost/prices_parsinDB')
+
+"""создаем таблицу в базе данных"""
 Base.metadata.create_all(engine)
+
+"""создаем сессию для работы с базой данных"""
 Session = sessionmaker(bind=engine)
 session = Session()
+
+"""создаем класс для модели данных"""
 
 
 class OilPrice(Base):
     __tablename__ = 'oil_prices'
-
     id = Column(Integer, primary_key=True)
     site = Column(String(100))
     oil_name = Column(String(100))
@@ -20,27 +32,45 @@ class OilPrice(Base):
     date = Column(Date)
 
 
-def save_prices(dict_prices):
-    for oil_name, price in dict_prices.items():
-        price = OilPrice(site='nature-express', oil_name=oil_name, price=price, date=datetime.now())
-        session.add(price)
-        session.commit()
+async def save_prices(dict_prices):
+    """Функция для сохранения цен в базе данных"""
+    with Session() as session:
+        for oil_name, price in dict_prices.items():
+            price = OilPrice(site='nature-express', oil_name=oil_name, price=price, date=datetime.now())
+            session.add(price)
+            session.commit()
 
 
-def view_prices(site=None, oil_name=None, price=None, max_price=None, start_date=None, end_date=None):
-    filtered_query = session.query(OilPrice)
-    if site:
-        self_query = filtered_query.filter(OilPrice.site == site)
-    if oil_name:
-        self_query = filtered_query.filter(OilPrice.oil_name == oil_name)
-    if price:
-        self_query = filtered_query.filter(OilPrice.price == price)
-    if max_price:
-        self_query = filtered_query.filter(OilPrice.price <= max_price)
-    if start_date:
-        self_query = filtered_query.filter(OilPrice.date >= start_date)
-    if end_date:
-        self_query = filtered_query.filter(OilPrice.date <= end_date)
-    return self_query.all()
+def view_prices(
+                site: str = None,
+                oil_name: str = None,
+                price: float = None,
+                max_price: float = None,
+                start_date: str = None,
+                end_date: str = None):
+    """Функция для просмотра цен в базе данных"""
+    with Session() as session:
+        query = session.query(OilPrice)
+        """
+        Проверка is not Nonе используется, чтобы избежать
+        интерпетации значения 0 как False.
+        filter_by фильтрует записи по конкретным значениям атрибутов
+        filter подходит для более сложной фильтрации
+        """
 
-
+        if site is not None:
+            query = query.filter_by(site=site)
+        if oil_name is not None:
+            query = query.filter(OilPrice.oil_name.ilike(f'%{oil_name}%'))
+        if price is not None:
+            query = query.filter_by(price=price)
+        if max_price is not None:
+            query = query.filter(OilPrice.price <= max_price)
+        if start_date is not None:
+            query = query.filter(OilPrice.date >= start_date)
+        if end_date is not None:
+            query = query.filter(OilPrice.date <= end_date)
+        print(f"in view_prices oil_name = {oil_name}")
+        print(f"in view_prices site = {site}")
+        prices = query.all()
+        return prices
--- main.py
+++ main.py
@@ -1,24 +1,65 @@
+import asyncio
+import aiohttp
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Form
 import pprint
 import time
 import requests
+from fastapi import Request
 from bs4 import BeautifulSoup
 from fastapi import FastAPI
-from BDalchemy import view_prices
-
+from BDalchemy import view_prices, save_prices
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
 
-@app.get('/')
+@app.post('/natureexpressprices')
 def prices():
-    return get_prices()
+    """API endpoint для получения цен на масла"""
+    return get_nature_express_prices()
 
-@app.get('/viewprices')
-def view():
-    return view_prices()
+@app.get('/', response_class=HTMLResponse)
+@app.get('/entry', response_class=HTMLResponse)
+async def view(request: Request):
+    """
+    Начальная страица.
+    API endpoint для получения сохраненных цен на масла.
+    response_class=HTMLResponse указывает,
+    что возвращаемое значение - HTML-страница
+    """
+    context = {"request": request, 'title': "Начальная страница"}
+    return templates.TemplateResponse('entry.html', context)
 
 
-def get_prices():
+@app.post('/viewprices', response_class=HTMLResponse)
+async def view(
+        request: Request,
+        site: str = Form(default=None),
+        oil_name: str = Form(default=None),
+        price: float = Form(default=None),
+        max_price: float = Form(default=None),
+        start_date: str = Form(default=None),
+        end_date: str = Form(default=None),):
+    """
+    API endpoint для получения сохраненных цен на масла.
+    response_class=HTMLResponse указывает,
+    что возвращаемое значение - HTML-страница
+    """
+    print(f'price =n {price}')
+    prices = view_prices(
+        site=site,
+        oil_name=oil_name,
+        price=price,
+        max_price=max_price,
+        start_date=start_date,
+        end_date=end_date, )
+    return templates.TemplateResponse('prices.html', {'request': request, 'prices': prices})
+
+
+
+def get_nature_express_prices():
     headers_chrome = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -34,7 +75,9 @@ def get_prices():
     }
     prefix_url = 'https://naturexpress.ru/katalog-tovarov/produktyi/rastitelnyie-masla/'
     address_site = 'https://naturexpress.ru/'
+    """Получаем страницу сайта"""
     page = requests.get(prefix_url, timeout=5, headers=headers_chrome)
+    """парсим страницу"""
     soup = BeautifulSoup(page.text, 'html.parser')
     links = soup.find_all('a')
     urls = []
@@ -47,35 +90,21 @@ def get_prices():
         time.sleep(1)
         page = requests.get(f'{address_site}{url}', timeout=5, headers=headers_mozila)
         soup = BeautifulSoup(page.content, 'html.parser')
-        """
-        Если необходимо получать данные из списка продуктов,
-        не обращаясь к каждому продукту, использовать этот код:
-        a_absolute = soup.find("div", class_="col-xs-6 col-sm-4").find('a', class_='absolute', href=True)['href'].find('rastitelnyie-masla')
-        print(a_absolute)
-        if a_absolute > 0:
-            oil_name = soup.find("div", class_="col-xs-6 col-sm-4").find('div', class_='title-h6').text.rstrip()
-            oil_name_prices[oil_name] = float(soup.find("div", class_="col-xs-6 col-sm-4").find('div', class_='price').text.strip().replace(' ', '').replace('₽', ''))
-            pprint.pprint(f'{url} | {oil_name} | {oil_name_prices[oil_name]}')
-        else:
-        """
         oil_name = soup.find('h1', class_="pagetitle", itemprop="name").text.strip()
-        oir_price = soup.find('div', class_="product-price-wrapper").find('span', class_="",  itemprop='price').text.rstrip().replace(' ₽', '')
+        oir_price = soup.find('div', class_="product-price-wrapper").find('span', class_="",
+                                                                          itemprop='price').text.rstrip().replace(' ₽', '')
+
         oil_name_prices[oil_name] = oir_price
-        #pprint.pprint(f'{url} | {oil_name} | {oil_name_prices[oil_name]}')
+        pprint.pprint(f'{url} | {oil_name} | {oil_name_prices[oil_name]}')
     if len(urls) == len(oil_name_prices):
         print('количество ссылок равно количеству запросов')
     else:
         print('Количество ссылок не равно количеству запросов')
-    #save_prices(oil_name_prices)
-    #return '\n'.join(f'{k}:{v}' for k, v in oil_name_prices.items())
+    save_prices(oil_name_prices)
+    return oil_name_prices
 
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app)
 
-"""
-https://naturexpress.ru/katalog-tovarov/produktyi/rastitelnyie-masla/amarantovoe-maslo-xolodnogo-otzhima-syirodavlennoe-50-ml
-https://naturexpress.ru/katalog-tovarov/produktyi/rastitelnyie-masla/maslo-chyornogo-tmina-250-ml
-https://naturexpress.ru/katalog-tovarov/produktyi/rastitelnyie-masla/maslo-chyornogo-tmina-250-ml
-"""
+    uvicorn.run(app)
