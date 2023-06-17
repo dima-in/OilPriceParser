7d85bf08481f674d56b2f4ab6cce23db60e2df5c
dima-in <dima-in@mail.ru>
2023-06-17 21:07:13 +0300

    рабочая версия для сайта nature-express

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from bs4 import BeautifulSoup
 from fastapi import FastAPI
 from BDalchemy import view_prices, save_prices
 
-
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 
@@ -24,7 +23,7 @@ def prices():
 
 def prices():
     """API endpoint для получения цен на масла"""
-    return   # get_maslodel_prices()
+    return  # get_maslodel_prices()
 
 
 @app.get('/', response_class=HTMLResponse)
@@ -63,67 +62,39 @@ async def view(
 
 
 def get_nature_express_prices():
-   headers_chrome = {
-       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
-   }
-   headers_mozila = {
-       "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'
-   }
-   prefix_url = 'https://naturexpress.ru/katalog-tovarov/produktyi/rastitelnyie-masla/'
-   address_site = 'https://naturexpress.ru/'
-   page = requests.get(prefix_url, timeout=5, headers=headers_chrome)  # Получаем страницу сайта
-   soup = BeautifulSoup(page.text, 'html.parser')  # парсим страницу
-   links = soup.find_all('a')  # получаем все ссылки с сайта
-   urls = []
-   for link in links:
-       link_str = str(link.get('href'))
-       if link_str.endswith('-ml'):
-           urls.append(link_str)
-   oil_name_prices = {}
-   for url in urls:
-       time.sleep(1)
-       page = requests.get(f'{address_site}{url}', timeout=5, headers=headers_mozila)
-       soup = BeautifulSoup(page.content, 'html.parser')
-       oil_name = soup.find('h1', class_="pagetitle", itemprop="name").text.strip()
-       oir_price = soup.find('div', class_="product-price-wrapper").find('span', class_="",
-                                                                         itemprop='price').text.rstrip().replace(' ₽', '')
-       oil_name_prices[oil_name] = oir_price
-       pprint.pprint(f'{url} | {oil_name} | {oil_name_prices[oil_name]}')
-   if len(urls) == len(oil_name_prices):
-       print('количество ссылок равно количеству запросов')
-   else:
-       print('Количество ссылок не равно количеству запросов')
-   save_prices(oil_name_prices)
-   return oil_name_prices
-
-"""
-@app.get('/maslodelprices')
-def get_maslodel_prices():
+    headers_chrome = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
+    }
     headers_mozila = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'
-            }
-    url = 'https://maslodel.zone/maslo_holodnogo_otzhima'
-    url_oblepiha = 'https://maslodel.zone/oblepihovoe_maslo'
-    url_address = 'https://maslodel.zone'
-    page = requests.get(url_oblepiha, headers=headers_mozila)  # получаем страницу сайта
-    soup = BeautifulSoup(page.content, 'html.parser')  # парсим страницу
-    #print(f'soup {soup}')
-    divs2 = soup.find_all('div', class_='js-store-grid-cont t-store__grid-cont t-container t-store__grid-cont_mobile-grid')
-    print(divs2)
-    for div in divs2:
-        print(div)
-
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'
+    }
+    prefix_url = 'https://naturexpress.ru/katalog-tovarov/produktyi/rastitelnyie-masla/'
+    address_site = 'https://naturexpress.ru/'
+    page = requests.get(prefix_url, timeout=5, headers=headers_chrome)  # Получаем страницу сайта
+    soup = BeautifulSoup(page.text, 'html.parser')  # парсим страницу
+    links = soup.find_all('a')  # получаем все ссылки с сайта
+    urls = []
+    for link in links:
+        link_str = str(link.get('href'))
+        if link_str.endswith('-ml'):
+            urls.append(link_str)
     oil_name_prices = {}
-    for product in divs2:
-        name = product.find('h4', class_='js-store-prod-name js-product-name t-store__card__title t-name t-name_xs').text
-        price = product.find('div', class_='js-product-price js-store-prod-price-val t-store__card__price-value notranslate').text.strip()
-        oil_name_prices[name] = float(price)
-        print(name)
-        print(price)
-    print(oil_name_prices)
-"""
-
-
+    for url in urls:
+        time.sleep(1)
+        page = requests.get(f'{address_site}{url}', timeout=5, headers=headers_mozila)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        oil_name = soup.find('h1', class_="pagetitle", itemprop="name").text.strip()
+        oir_price = soup.find('div', class_="product-price-wrapper").find('span', class_="",
+                                                                          itemprop='price').text.rstrip().replace(' ₽',
+                                                                                                                  '')
+        oil_name_prices[oil_name] = oir_price
+        pprint.pprint(f'{url} | {oil_name} | {oil_name_prices[oil_name]}')
+    if len(urls) == len(oil_name_prices):
+        print('количество ссылок равно количеству запросов')
+    else:
+        print('Количество ссылок не равно количеству запросов')
+    save_prices(oil_name_prices)
+    return oil_name_prices
 
 
 if __name__ == '__main__':
